9cf0ad05473e5908594a5278d95c301785d0c967
Catmo8 <catmo8@cox.net>
2019-12-02 14:55:33 -0600

    Update LoginPage.xaml.cs
    Alan you can take over. at doctors

--- MentorMeet/MentorMeet/Views/LoginPage.xaml.cs
+++ MentorMeet/MentorMeet/Views/LoginPage.xaml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MentorMeet.Users;
+using MentorMeet.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using SQLite;
+using System.IO;
 
 namespace MentorMeet.Views
 {
@@ -20,6 +22,7 @@ namespace MentorMeet.Views
         //response after login button clicked
         async void LoginClicked(object sender, System.EventArgs e)
         {
+            /*
             var item = new User(UsernameEntry.Text, PasswordEntry.Text);
 
             //check for empty imputs
@@ -37,6 +40,17 @@ namespace MentorMeet.Views
                 textToDisplay = "Enter LSUID (with @lsu.edu) to login";
                 
             }
+            */
+
+            using (SQLiteConnection conn = new SQLiteConnection(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MentorMeetSQLite.db3")))
+            {
+                var account = conn.Table<User>().ToList();
+            }
+            if (!account.Contains(UsernameEntry.Text))
+            {
+                DisplayAlert("Message", "Username o)
+            }
+
             await Navigation.PushModalAsync(new MainPage());
             
         }
@@ -45,7 +59,6 @@ namespace MentorMeet.Views
         async void SignUpClicked(object sender, System.EventArgs e)
         {
             await Navigation.PushModalAsync(new SignUpPage());
-
         }
 
         async void databaseClicked(object sender, System.EventArgs e)
