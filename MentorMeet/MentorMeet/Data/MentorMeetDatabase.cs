088445bc735fbe5af35cee33c7e5d35c2c912438
Catmo8 <catmo8@cox.net>
2019-12-04 13:44:28 -0600

    Update MentorMeetDatabase.cs

--- MentorMeet/MentorMeet/Data/MentorMeetDatabase.cs
+++ MentorMeet/MentorMeet/Data/MentorMeetDatabase.cs
@@ -15,8 +15,12 @@ namespace MentorMeet.Data
         {
             database = new SQLiteAsyncConnection(dbPath);
             database.CreateTableAsync<User>().Wait();
+            database.CreateTableAsync<Matches>().Wait();
+            database.CreateTableAsync<Matching>().Wait();
+            database.CreateTableAsync<Messages>().Wait();
         }
 
+        #region User
         public Task<List<User>> UserGetItemsAsync()
         {
             return database.Table<User>().ToListAsync();
@@ -39,9 +43,100 @@ namespace MentorMeet.Data
             }
         }
 
-        //public Task<int> DeleteItemAsync(User item)
+        //public Task<int> UserDeleteItemAsync(User item)
         //{
         //    return database.DeleteAsync(item);
         //}
+        #endregion
+
+        #region Matches
+        public Task<List<Matches>> MatchesGetItemsAsync()
+        {
+            return database.Table<Matches>().ToListAsync();
+        }
+
+        public Task<Matches> MatchesGetItemAsync(int id)
+        {
+            return database.Table<Matches>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> MatchesSaveItemAsync(Matches item)
+        {
+            if (item.Id != 0)
+            {
+                return database.UpdateAsync(item);
+            }
+            else
+            {
+                return database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(Matches item)
+        {
+            return database.DeleteAsync(item);
+        }
+
+        #endregion
+
+        #region Matching
+        public Task<List<Matching>> MatchingGetItemsAsync()
+        {
+            return database.Table<Matching>().ToListAsync();
+        }
+
+        public Task<Matching> MatchingGetItemAsync(int id)
+        {
+            return database.Table<Matching>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> MatchingSaveItemAsync(Matching item)
+        {
+            if (item.Id != 0)
+            {
+                return database.UpdateAsync(item);
+            }
+            else
+            {
+                return database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> MatchingDeleteItemAsync(Matching item)
+        {
+            return database.DeleteAsync(item);
+        }
+
+        #endregion
+
+        #region Messages
+        public Task<List<Messages>> MessagesGetItemsAsync()
+        {
+            return database.Table<Messages>().ToListAsync();
+        }
+
+        public Task<Messages> MessagesGetItemAsync(int id)
+        {
+            return database.Table<Messages>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> MessagesSaveItemAsync(Messages item)
+        {
+            if (item.Id != 0)
+            {
+                return database.UpdateAsync(item);
+            }
+            else
+            {
+                return database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> MessagesDeleteItemAsync(Messages item)
+        {
+            return database.DeleteAsync(item);
+        }
+
+        #endregion
     }
 }
